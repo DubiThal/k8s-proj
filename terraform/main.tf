d09d731b9401629eaff03d5765b437c97bf8a2e3
Dubi Thal <dubithal@gmail.com>
2025-11-04 19:12:53 +0200

    still jenkins init container issues

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     config_path = "~/.kube/config"
   }
 }
@@ -26,25 +26,36 @@ resource "kubernetes_namespace" "jenkins" {
   }
 }
 
-# resource "helm_release" "prometheus" {
-#   name             = "prometheus"
-#   repository       = "https://prometheus-community.github.io/helm-charts"
-#   chart            = "prometheus"
-#   namespace        = kubernetes_namespace.monitoring.metadata[0].name
-#   create_namespace = false
-#   values           = [file("${path.module}/prometheus-values.yaml")]
-#   timeout    = 90
-#   depends_on       = [kubernetes_namespace.monitoring]
-# }
+resource "kubernetes_namespace" "local_path_storage" {
+  metadata {
+    name = "local-path-storage"
+  }
+}
 
-#resource "helm_release" "grafana" {
-#  name       = "grafana"
-#  repository = "https://grafana.github.io/helm-charts"
-#  chart      = "grafana"
-#  namespace  = kubernetes_namespace.monitoring.metadata[0].name
-#  values     = [file("${path.module}/grafana-values.yaml")]
-#  depends_on = [kubernetes_namespace.monitoring]
-#}
+ resource "helm_release" "prometheus" {
+   name             = "prometheus"
+   repository       = "https://prometheus-community.github.io/helm-charts"
+   chart            = "prometheus"
+   namespace        = kubernetes_namespace.monitoring.metadata[0].name
+   create_namespace = false
+   values           = [file("${path.module}/prometheus-values.yaml")]
+   depends_on = [
+     kubernetes_namespace.monitoring,
+     kubernetes_manifest.local_path_provisioner
+   ]
+ }
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  values     = [file("${path.module}/grafana-values.yaml")]
+  depends_on = [
+    kubernetes_namespace.monitoring,
+    kubernetes_manifest.local_path_provisioner
+  ]
+}
 
 resource "helm_release" "vault" {
   name       = "vault"
@@ -52,7 +63,33 @@ resource "helm_release" "vault" {
   chart      = "vault"
   namespace  = kubernetes_namespace.vault.metadata[0].name
   values     = [file("${path.module}/vault-values.yaml")]
-  depends_on = [kubernetes_namespace.vault]
+  depends_on = [
+    kubernetes_namespace.vault,
+    kubernetes_manifest.local_path_provisioner
+  ]
+}
+
+locals {
+  # Split the multi-document YAML into a list of individual YAML document strings
+  local_path_manifest_strings = split("---", data.http.local_path_storage.response_body)
+}
+
+resource "kubernetes_manifest" "local_path_provisioner" {
+  # Iterate over the list of YAML strings, filtering out any empty ones
+  for_each = {
+    for i, s in local.local_path_manifest_strings : i => s
+    if trimspace(s) != "" && !startswith(yamldecode(s).kind, "Namespace")
+  }
+  manifest = yamldecode(each.value)
+  depends_on = [
+    kubernetes_namespace.local_path_storage
+  ]
+}
+
+data "http" "local_path_storage" {
+  url = "https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.26/deploy/local-path-storage.yaml"
+  # Optional: Add a checksum to ensure the file integrity
+  # request_checksum = "sha256:..."
 }
 
 resource "helm_release" "jenkins" {
@@ -61,6 +98,9 @@ resource "helm_release" "jenkins" {
   chart      = "jenkins"
   namespace  = kubernetes_namespace.jenkins.metadata[0].name
   values     = [file("${path.module}/jenkins-values.yaml")]
-  timeout    = 600
-  depends_on = [kubernetes_namespace.jenkins]
+  timeout    = 100
+  depends_on = [
+    kubernetes_namespace.jenkins,
+    kubernetes_manifest.local_path_provisioner
+  ]
 }
